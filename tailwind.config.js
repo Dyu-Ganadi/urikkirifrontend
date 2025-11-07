abac4cc3e3c35591faae3fa008b3a094dc268ae9
김소희 <ttohee1275@gmail.com>
2025-11-07 12:04:45 +0900

    chore: 컬러 시스템 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,28 @@ export default {
         sans: ["TMoneyDungunbaram", ...defaultTheme.fontFamily.sans],
         pretendard: ["Pretendard"],
       },
+      colors: {
+        main: {
+          1: "#FF9B00",
+          2: "#FFC900",
+          3: "#FFE103",
+          4: "#FFF09A",
+          5: "#FFECBE",
+        },
+        system: {
+          error: "#E92020",
+          success: "#1DD923",
+          blue: "#209CF5",
+        },
+        mono: {
+          1: "#121212",
+          2: "#5E5E5E",
+          3: "#989898",
+          4: "#CACACA",
+          5: "#EBEBEB",
+          6: "#F5F5F5",
+        },
+      },
     },
   },
   plugins: [],
