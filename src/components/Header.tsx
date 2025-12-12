d79d9d0ebf6bfcb94227a5be790c4c8b0de2ae7d
김소희 <ttohee1275@gmail.com>
2025-12-12 10:50:35 +0900

    feat: 이미지 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import person_icon from "../assets/icon/person_icon.svg";
+import person_icon from "../assets/icon/person-icon-white.svg";
 import exit from "../assets/icon/exit.svg";
 import { useNavigate } from "react-router-dom";
 
@@ -11,7 +11,7 @@ const Header = ({ isAuth }: Prop) => {
   const [clickProfile, setClickProfile] = useState(false);
   const navigate = useNavigate();
   return (
-    <div className="w-screen pl-10 pr-14 py-[19px] border-b border-b-mono-4 flex flex-row justify-between items-center">
+    <div className="w-screen pl-10 pr-14 py-[19px] bg-white border-b border-b-mono-4 flex flex-row justify-between items-center">
       <h1 className="text-main-1 text-4xl" onClick={() => navigate("/")}>
         우리끼리
       </h1>
@@ -27,6 +27,9 @@ const Header = ({ isAuth }: Prop) => {
             <div className="absolute right-24 top-16 w-[260px] rounded-[20px] border border-mono-3 overflow-hidden bg-white flex flex-col">
               <div className="flex justify-center py-5 text-[25px] border-b border-mono-3">
                 마이페이지
+              </div>
+              <div className="flex justify-center py-5 text-[25px] border-b border-mono-3">
+                랭킹페이지
               </div>
               <div className="flex flex-row gap-2 justify-center py-5 text-[25px] text-system-error">
                 로그아웃
