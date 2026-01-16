aaba15d1bf10473aefc5510d424c0ec8b7ba8ee3
김소희 <ttohee1275@gmail.com>
2026-01-16 12:17:51 +0900

    feat: 헤더 숨기기 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, useLocation } from "react-router-dom";
 import { Header } from "./components/index.ts";
 import {
   LandingPage,
@@ -10,16 +10,24 @@ import {
   EnterRoom,
   WaitingRoom,
 } from "./pages/index.ts";
+import { WebSocketProvider } from "./context/WebSocketContext.tsx";
 
 const Layout = () => {
   const token = localStorage.getItem("access_token");
   const isAuth = token !== null && token !== "undefined" && token !== "";
+  const location = useLocation();
+
+  // 대기 화면에서는 헤더 숨기기
+  const hideHeader = location.pathname === "/wait-room";
+
   return (
     <>
       <div className="relative flex flex-col min-h-screen">
-        <div className="z-20 absolute top-0 left-0">
-          <Header isAuth={isAuth} />
-        </div>
+        {!hideHeader && (
+          <div className="z-20 absolute top-0 left-0">
+            <Header isAuth={isAuth} />
+          </div>
+        )}
 
         <Outlet />
       </div>
@@ -29,20 +37,22 @@ const Layout = () => {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/game" element={<GamePage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/ranking" element={<RankingPage />} />
-          <Route path="/enter-room" element={<EnterRoom />} />
-          <Route path="/wait-room" element={<WaitingRoom />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <WebSocketProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/game" element={<GamePage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/ranking" element={<RankingPage />} />
+            <Route path="/enter-room" element={<EnterRoom />} />
+            <Route path="/wait-room" element={<WaitingRoom />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </WebSocketProvider>
   );
 }
 
